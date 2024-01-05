3762d794f4ece52733241ac4bb028bbdd5af42d2
Anubhab Mukherjee <anubhabmukherjee5@gmail.com>
2024-01-05 15:42:21 +0530

    add js file

--- js/app.js
+++ js/app.js
@@ -14,16 +14,66 @@ function addValues(num1, num2) {
     console.log(num1 + num2);
 }
 
-addValues("rahul", 4);
+// addValues("Rahul", 4);
 
-function mouseEnterFn() {
-    console.log("mouse enter");
-}
+// function mouseEnterFn() {
+//     console.log("mouse enter");
+// }
 
-function mouseLeaveFn() {
-    console.log("mouse Leave called");
-}
+// function mouseLeaveFn() {
+//     console.log("mouse Leave called");
+// }
 
-function mouseClickFn() {
-    console.log("mouse clicked");
-}
+// function mouseClickFn() {
+//     console.log("mouse clicked");
+// }
+
+// let myMarks = 12;
+// console.log(myMarks)
+
+
+// myFunc();
+
+// function myFunc() {
+
+//     console.log('myFunc is called');
+
+//     // if (true) {
+//     //     const age = 23;
+//     //     console.log(age);
+//     // }
+
+//     // console.log(age);
+// }
+
+
+// var myFunc = function () {
+//     myFunc();
+//     console.log("my func is called");
+// }
+
+// (function getMyName() {
+//     console.log('My Name Is Ak');
+// })();
+
+let student = {
+    sName: "Abc",
+    sAge: 23,
+    sMarks: [20, 12, 23],
+    sIsPassed: true,
+    sAddress: {
+        houseNumber: "204"
+    },
+    sayMyName: function () {
+        console.log("My name is Abc");
+    }
+};
+console.log(student.sMarks[1])
+// console.log(student.sayMyName());
+
+let propName = 'sName';
+console.log(student[propName]);
+
+const arr = [1, 2, 3, 4];
+
+console.log(arr[20]);
